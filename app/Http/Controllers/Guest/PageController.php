ddd5206f1ad5d7561c610fe5965939e48dc7e417
benitorubano1992 <rubanobenito@gmail.com>
2022-12-20 16:06:41 +0100

    complete task

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -11,7 +11,7 @@ class PageController extends Controller
     public function index()
     {
 
-        $trains = Train::where('orario_partenza', '<=', '2022-12-19 23:59:59')->get();
+        $trains = Train::where('orario_partenza', '>=', now())->get();
 
 
         return view("trains.index", compact('trains'));
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();   //
+            $newTrain->azineda = $faker->word();
+            $newTrain->stazione_partenza = $faker->city();
+            $newTrain->stazione_arrivo = $faker->city();
+            $newTrain->orario_partenza = $faker->dateTimeBetween('+0 days', '+5 days');
+            $newTrain->orario_arrivo = $faker->dateTimeBetween('+0 days', '+5 days');
+            $newTrain->codice = $faker->bothify("??####");
+            $newTrain->carrozze = $faker->numberBetween(1, 10);
+            $newTrain->in_orario = $faker->numberBetween(0, 1);
+            $newTrain->cancellato = $faker->numberBetween(0, 1);
+            $newTrain->save();
+        }
+    }
+}
